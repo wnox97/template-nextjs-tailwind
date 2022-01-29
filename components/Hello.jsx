d32c68176e014bad5b4f033e15c98ec3a161ef6e
Wilmer Montilla <wilmermontilla@8.0.16.172.net-uno.net>
2022-01-29 19:23:50 -0400

    jsconfig absolute paths

---
+++ components/Hello.jsx
@@ -0,0 +1,7 @@
+export default function Hello() {
+  return (
+    <div className="w-full">
+      <h1 className="text-gray-700 text-4xl">hola</h1>
+    </div>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Hello from 'components/Hello';
 
 export default function Home() {
   return (
-    <div className="w-full min-h-screen">
-      <h1 className="text-3xl text-gray-700">Next Js with tailwind</h1>
+    <div className="w-full flex flex-col">
+      <h1 className="text-4xl text-gray-700">NextJS with tailwind</h1>
+      <Hello />
     </div>
   );
 }
